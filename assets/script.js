2776561d10c0e63f92a7d8a486ee61126274b6a7
ColeW <HashBrownBrother024@gmail.com>
2023-10-09 08:59:49 -0400

    added the javascript to the assets folder

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,11 @@
- var topics = "HTML, CSS, Git, JavaScript";
- console.log(topics);
+var topic = "HTML"; {
+    console.log("Let's Study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Lets's Study CSS!");
+} else if (topic === 'Git') {
+    console.log("Let's study Git!");    
+} else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript");
+} else {
+    console.log;('Please try again!');
+}    
