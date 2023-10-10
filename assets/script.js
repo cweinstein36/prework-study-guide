6201a3b2b0e2c1976f6e7a021155394b70fa1620
ColeW <HashBrownBrother024@gmail.com>
2023-10-10 11:16:05 -0400

    javascript try #2

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,13 @@
- var topic = "HTML"; {
-    console.log("Let's Study HTML!");
- } else if (topic === 'CSS') {
-    console.log("Lets's Study CSS!");
- } else if (topic === 'Git') {
-    console.log("Let's study Git!");    
- } else if (topic === 'JavaScript') {
-    console.log("Let's study JavaScript");
- } else {
-    console.log;('Please try again!');
- }    
+var topic = "HTML";
+
+if (topic === 'HTML') {
+ console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+ console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+ console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+ console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!');
+}
